ecd9dbd3f17bceeb323042935fbf8e89a43db178
loBoda0 <martin.loboda02@gmail.com>
2024-01-09 11:53:39 +0100

    auction tags functionality

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,8 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 export const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
+    ,
 )
